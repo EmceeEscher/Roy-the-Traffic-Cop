46dd61d4db9aef5a4f9ee72479ccc8953f4b9114
Jacob Brunette <brunette.jacob18@gmail.com>
2018-01-21 14:56:56 -0800

    initial setup

---
+++ cmake-build-debug/CMakeFiles/template.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/jacob/projects/cs436d/template/src/a1.cpp" "/Users/jacob/projects/cs436d/template/cmake-build-debug/CMakeFiles/template.dir/src/a1.cpp.o"
+  "/Users/jacob/projects/cs436d/template/src/common.cpp" "/Users/jacob/projects/cs436d/template/cmake-build-debug/CMakeFiles/template.dir/src/common.cpp.o"
+  "/Users/jacob/projects/cs436d/template/src/fish.cpp" "/Users/jacob/projects/cs436d/template/cmake-build-debug/CMakeFiles/template.dir/src/fish.cpp.o"
+  "/Users/jacob/projects/cs436d/template/src/salmon.cpp" "/Users/jacob/projects/cs436d/template/cmake-build-debug/CMakeFiles/template.dir/src/salmon.cpp.o"
+  "/Users/jacob/projects/cs436d/template/src/turtle.cpp" "/Users/jacob/projects/cs436d/template/cmake-build-debug/CMakeFiles/template.dir/src/turtle.cpp.o"
+  "/Users/jacob/projects/cs436d/template/src/world.cpp" "/Users/jacob/projects/cs436d/template/cmake-build-debug/CMakeFiles/template.dir/src/world.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
