a75213acf0a4c8677cfc082d5c8cde266f819f3b
Jacob Brunette <thisisnotan@email.com>
2018-02-21 11:36:55 -0800

    changed vcxproj

--- src/placard.cpp
+++ src/placard.cpp
@@ -160,7 +160,7 @@ void Placard::start_timer(float max_time) {
 
 void Placard::set_rotation(float parent_rotation) {
   m_rotation = parent_rotation + M_PI / 2;
-  
+
   m_position.x = m_position.x + cos(m_rotation) * m_offset_from_parent;
   m_position.y = m_position.y + sin(m_rotation) * m_offset_from_parent;
 }
