4412566154df51df26b47ce15a267497b90de519
EmceeEscher <test@email.com>
2018-03-19 16:28:29 -0700

    got it to compile

--- src/score_display.cpp
+++ src/score_display.cpp
@@ -3,9 +3,9 @@
 Texture ScoreDisplay::score_tex;
 TexturedVertex2 score_vertices[36];
 
-float uv;
-float i_w;
-float s_p;
+float score_uv;
+float score_i_w;
+float score_s_p;
 
 int d0_flip = 0;
 int d1_flip = 0;
@@ -14,7 +14,7 @@ int d2_flip = 0;
 gt_tracker gt_score;
 
 float d0_shown_offset;
-float d1_shown_offest;
+float d1_shown_offset;
 float d2_shown_offset;
 
 bool ScoreDisplay::init()
@@ -32,101 +32,101 @@ bool ScoreDisplay::init()
 	}
 
 	//centre of the texture
-	uv = 100.f / score_tex.width; //texture uv
-	i_w = score_tex.width / 10.f; //individual number width
-	s_p = -600.f; //starting position
+	score_uv = 100.f / score_tex.width; //texture score_uv
+	score_i_w = score_tex.width / 10.f; //individual number width
+	score_s_p = -600.f; //starting position
 	float hr = score_tex.height * 0.5;
 
 	//digit 0 top -- new
-	score_vertices[0].position = { s_p + i_w	    , 0  , 0.f };
-	score_vertices[1].position = { s_p + 2 * i_w  , 0  , 0.f };
-	score_vertices[2].position = { s_p + 2 * i_w	, -hr, 0.f };
-	score_vertices[3].position = { s_p + i_w		, -hr, 0.f };
-	score_vertices[0].texcoord = { uv * 0.f, 0.5  , 0.1f };
-	score_vertices[1].texcoord = { uv * 1.f, 0.5  , 0.1f };
-	score_vertices[2].texcoord = { uv * 1.f, 0.0f , 0.1f };
-	score_vertices[3].texcoord = { uv * 0.f, 0.0f , 0.1f };
+	score_vertices[0].position = { score_s_p + score_i_w	    , 0  , 0.f };
+	score_vertices[1].position = { score_s_p + 2 * score_i_w  , 0  , 0.f };
+	score_vertices[2].position = { score_s_p + 2 * score_i_w	, -hr, 0.f };
+	score_vertices[3].position = { score_s_p + score_i_w		, -hr, 0.f };
+	score_vertices[0].texcoord = { score_uv * 0.f, 0.5  , 0.1f };
+	score_vertices[1].texcoord = { score_uv * 1.f, 0.5  , 0.1f };
+	score_vertices[2].texcoord = { score_uv * 1.f, 0.0f , 0.1f };
+	score_vertices[3].texcoord = { score_uv * 0.f, 0.0f , 0.1f };
 
 	//digit 0 bottom -- old
-	score_vertices[4].position = { s_p + i_w		, +hr, 0.f };
-	score_vertices[5].position = { s_p + 2 * i_w   , +hr, 0.f };
-	score_vertices[6].position = { s_p + 2 * i_w	, 0, 0.f };
-	score_vertices[7].position = { s_p + i_w		, 0, 0.f };
-	score_vertices[4].texcoord = { uv * 0.f, 1.0f,  0.11f };
-	score_vertices[5].texcoord = { uv * 1.f, 1.0f,  0.11f };
-	score_vertices[6].texcoord = { uv * 1.f, 0.5f, 0.11f };
-	score_vertices[7].texcoord = { uv * 0.f, 0.5f,  0.11f };
+	score_vertices[4].position = { score_s_p + score_i_w		, +hr, 0.f };
+	score_vertices[5].position = { score_s_p + 2 * score_i_w   , +hr, 0.f };
+	score_vertices[6].position = { score_s_p + 2 * score_i_w	, 0, 0.f };
+	score_vertices[7].position = { score_s_p + score_i_w		, 0, 0.f };
+	score_vertices[4].texcoord = { score_uv * 0.f, 1.0f,  0.11f };
+	score_vertices[5].texcoord = { score_uv * 1.f, 1.0f,  0.11f };
+	score_vertices[6].texcoord = { score_uv * 1.f, 0.5f, 0.11f };
+	score_vertices[7].texcoord = { score_uv * 0.f, 0.5f,  0.11f };
 
 	//display for digit 0 -- rotating
-	score_vertices[8].position = { s_p + i_w		 , +hr, 0.1f };
-	score_vertices[9].position = { s_p + 2 * i_w  , +hr, 0.1f };
-	score_vertices[10].position = { s_p + 2 * i_w	 , -hr, 0.1f };
-	score_vertices[11].position = { s_p + i_w		 , -hr, 0.1f };
-	score_vertices[8].texcoord = { uv * 0, 1.0f, 1.0f };
-	score_vertices[9].texcoord = { uv * 1, 1.0f, 1.0f };
-	score_vertices[10].texcoord = { uv * 1, 0.0f, 1.0f };
-	score_vertices[11].texcoord = { uv * 0, 0.0f, 1.0f };
+	score_vertices[8].position = { score_s_p + score_i_w		 , +hr, 0.1f };
+	score_vertices[9].position = { score_s_p + 2 * score_i_w  , +hr, 0.1f };
+	score_vertices[10].position = { score_s_p + 2 * score_i_w	 , -hr, 0.1f };
+	score_vertices[11].position = { score_s_p + score_i_w		 , -hr, 0.1f };
+	score_vertices[8].texcoord = { score_uv * 0, 1.0f, 1.0f };
+	score_vertices[9].texcoord = { score_uv * 1, 1.0f, 1.0f };
+	score_vertices[10].texcoord = { score_uv * 1, 0.0f, 1.0f };
+	score_vertices[11].texcoord = { score_uv * 0, 0.0f, 1.0f };
 
 
 	//digit 1 top -- new
-	score_vertices[12].position = { s_p      , 0  , 0.f };
-	score_vertices[13].position = { s_p + i_w, 0  , 0.f };
-	score_vertices[14].position = { s_p + i_w, -hr, 0.f };
-	score_vertices[15].position = { s_p      , -hr, 0.f };
-	score_vertices[12].texcoord = { uv  * 0.f, 0.5f, 0.2f };
-	score_vertices[13].texcoord = { uv  * 1.f, 0.5f, 0.2f };
-	score_vertices[14].texcoord = { uv  * 1.f, 0.0f, 0.2f };
-	score_vertices[15].texcoord = { uv  * 0.f, 0.0f, 0.2f };
+	score_vertices[12].position = { score_s_p      , 0  , 0.f };
+	score_vertices[13].position = { score_s_p + score_i_w, 0  , 0.f };
+	score_vertices[14].position = { score_s_p + score_i_w, -hr, 0.f };
+	score_vertices[15].position = { score_s_p      , -hr, 0.f };
+	score_vertices[12].texcoord = { score_uv  * 0.f, 0.5f, 0.2f };
+	score_vertices[13].texcoord = { score_uv  * 1.f, 0.5f, 0.2f };
+	score_vertices[14].texcoord = { score_uv  * 1.f, 0.0f, 0.2f };
+	score_vertices[15].texcoord = { score_uv  * 0.f, 0.0f, 0.2f };
 
 	//digit 1 bottom -- old
-	score_vertices[16].position = { s_p      , +hr , 0.f };
-	score_vertices[17].position = { s_p + i_w, +hr , 0.f };
-	score_vertices[18].position = { s_p + i_w, 0	 , 0.f };
-	score_vertices[19].position = { s_p      , 0	 , 0.f };
-	score_vertices[16].texcoord = { uv  * 0.f, 1.0f, 0.22f };
-	score_vertices[17].texcoord = { uv  * 1.f, 1.0f, 0.22f };
-	score_vertices[18].texcoord = { uv  * 1.f, 0.5f, 0.22f };
-	score_vertices[19].texcoord = { uv  * 0.f, 0.5f, 0.22f };
+	score_vertices[16].position = { score_s_p      , +hr , 0.f };
+	score_vertices[17].position = { score_s_p + score_i_w, +hr , 0.f };
+	score_vertices[18].position = { score_s_p + score_i_w, 0	 , 0.f };
+	score_vertices[19].position = { score_s_p      , 0	 , 0.f };
+	score_vertices[16].texcoord = { score_uv  * 0.f, 1.0f, 0.22f };
+	score_vertices[17].texcoord = { score_uv  * 1.f, 1.0f, 0.22f };
+	score_vertices[18].texcoord = { score_uv  * 1.f, 0.5f, 0.22f };
+	score_vertices[19].texcoord = { score_uv  * 0.f, 0.5f, 0.22f };
 
 	//display for digit 1 -- rotating
-	score_vertices[20].position = { s_p 	, +hr, 0.2f };
-	score_vertices[21].position = { s_p + i_w , +hr, 0.2f };
-	score_vertices[22].position = { s_p + i_w	, -hr, 0.2f };
-	score_vertices[23].position = { s_p	, -hr, 0.2f };
-	score_vertices[20].texcoord = { uv * 0, 1.0f, 2.0f };
-	score_vertices[21].texcoord = { uv * 1, 1.0f, 2.0f };
-	score_vertices[22].texcoord = { uv * 1, 0.0f, 2.0f };
-	score_vertices[23].texcoord = { uv * 0, 0.0f, 2.0f };
+	score_vertices[20].position = { score_s_p 	, +hr, 0.2f };
+	score_vertices[21].position = { score_s_p + score_i_w , +hr, 0.2f };
+	score_vertices[22].position = { score_s_p + score_i_w	, -hr, 0.2f };
+	score_vertices[23].position = { score_s_p	, -hr, 0.2f };
+	score_vertices[20].texcoord = { score_uv * 0, 1.0f, 2.0f };
+	score_vertices[21].texcoord = { score_uv * 1, 1.0f, 2.0f };
+	score_vertices[22].texcoord = { score_uv * 1, 0.0f, 2.0f };
+	score_vertices[23].texcoord = { score_uv * 0, 0.0f, 2.0f };
 
 	//digit 2 top -- new
-	score_vertices[24].position = { s_p + 3.5f * i_w	, 0  , 0.f };
-	score_vertices[25].position = { s_p + 4.5f * i_w , 0  , 0.f };
-	score_vertices[26].position = { s_p + 4.5f * i_w	, -hr, 0.f };
-	score_vertices[27].position = { s_p + 3.5f * i_w	, -hr, 0.f };
-	score_vertices[24].texcoord = { uv * 0.f, 0.5  , 0.3f };
-	score_vertices[25].texcoord = { uv * 1.f, 0.5  , 0.3f };
-	score_vertices[26].texcoord = { uv * 1.f, 0.0f , 0.3f };
-	score_vertices[27].texcoord = { uv * 0.f, 0.0f , 0.3f };
+	score_vertices[24].position = { score_s_p + 3.5f * score_i_w	, 0  , 0.f };
+	score_vertices[25].position = { score_s_p + 4.5f * score_i_w , 0  , 0.f };
+	score_vertices[26].position = { score_s_p + 4.5f * score_i_w	, -hr, 0.f };
+	score_vertices[27].position = { score_s_p + 3.5f * score_i_w	, -hr, 0.f };
+	score_vertices[24].texcoord = { score_uv * 0.f, 0.5  , 0.3f };
+	score_vertices[25].texcoord = { score_uv * 1.f, 0.5  , 0.3f };
+	score_vertices[26].texcoord = { score_uv * 1.f, 0.0f , 0.3f };
+	score_vertices[27].texcoord = { score_uv * 0.f, 0.0f , 0.3f };
 
 	//digit 2 bottom -- old
-	score_vertices[28].position = { s_p + 3.5f * i_w	, +hr, 0.f };
-	score_vertices[29].position = { s_p + 4.5f * i_w , +hr, 0.f };
-	score_vertices[30].position = { s_p + 4.5f * i_w	, 0, 0.f };
-	score_vertices[31].position = { s_p + 3.5f * i_w	, 0, 0.f };
-	score_vertices[28].texcoord = { uv * 0.f, 1.0f,  0.33f };
-	score_vertices[29].texcoord = { uv * 1.f, 1.0f,  0.33f };
-	score_vertices[30].texcoord = { uv * 1.f, 0.5f,  0.33f };
-	score_vertices[31].texcoord = { uv * 0.f, 0.5f,  0.33f };
+	score_vertices[28].position = { score_s_p + 3.5f * score_i_w	, +hr, 0.f };
+	score_vertices[29].position = { score_s_p + 4.5f * score_i_w , +hr, 0.f };
+	score_vertices[30].position = { score_s_p + 4.5f * score_i_w	, 0, 0.f };
+	score_vertices[31].position = { score_s_p + 3.5f * score_i_w	, 0, 0.f };
+	score_vertices[28].texcoord = { score_uv * 0.f, 1.0f,  0.33f };
+	score_vertices[29].texcoord = { score_uv * 1.f, 1.0f,  0.33f };
+	score_vertices[30].texcoord = { score_uv * 1.f, 0.5f,  0.33f };
+	score_vertices[31].texcoord = { score_uv * 0.f, 0.5f,  0.33f };
 
 	//display for digit 2 -- rotating
-	score_vertices[32].position = { s_p + 3.5f * i_w	, +hr, 0.3f };
-	score_vertices[33].position = { s_p + 4.5f * i_w , +hr, 0.3f };
-	score_vertices[34].position = { s_p + 4.5f * i_w	, -hr, 0.3f };
-	score_vertices[35].position = { s_p + 3.5f * i_w	, -hr, 0.3f };
-	score_vertices[32].texcoord = { uv * 0, 1.0f, 3.0f };
-	score_vertices[33].texcoord = { uv * 1, 1.0f, 3.0f };
-	score_vertices[34].texcoord = { uv * 1, 0.0f, 3.0f };
-	score_vertices[35].texcoord = { uv * 0, 0.0f, 3.0f };
+	score_vertices[32].position = { score_s_p + 3.5f * score_i_w	, +hr, 0.3f };
+	score_vertices[33].position = { score_s_p + 4.5f * score_i_w , +hr, 0.3f };
+	score_vertices[34].position = { score_s_p + 4.5f * score_i_w	, -hr, 0.3f };
+	score_vertices[35].position = { score_s_p + 3.5f * score_i_w	, -hr, 0.3f };
+	score_vertices[32].texcoord = { score_uv * 0, 1.0f, 3.0f };
+	score_vertices[33].texcoord = { score_uv * 1, 1.0f, 3.0f };
+	score_vertices[34].texcoord = { score_uv * 1, 0.0f, 3.0f };
+	score_vertices[35].texcoord = { score_uv * 0, 0.0f, 3.0f };
 
 
 	uint16_t indices[] = { 0,3,1,1,3,2,4,7,5,5,7,6,8,11,9,9,11,10,
@@ -155,33 +155,33 @@ bool ScoreDisplay::init()
 	m_position = { 215.f, 80.f };
 
 	gt_score.digit_0.old_offset = 0.0f;
-	gt_score.digit_0.new_offset = uv * 1;
+	gt_score.digit_0.new_offset = score_uv * 1;
 	gt_score.digit_0.flip = -1;
-	score_d0_shown_offset = uv * 1;
+	d0_shown_offset = score_uv * 1;
 
 	gt_score.digit_1.old_offset = 0.0f;
 	gt_score.digit_1.new_offset = 0.0f;
 	gt_score.digit_1.flip = -1;
-	score_d1_shown_offset = 0.f;
+	d1_shown_offset = 0.f;
 
 	gt_score.digit_2.old_offset = 0.0f;
-	gt_score.digit_2.new_offset = uv * 2;
+	gt_score.digit_2.new_offset = score_uv * 2;
 	gt_score.digit_2.flip = -1;
-	score_d2_shown_offset = uv * 2;
+	d2_shown_offset = score_uv * 2;
 
 	return true;
 }
 
 void ScoreDisplay::SplitSetScoreDigits(int score, gt_tracker* gt_points) {
-	float score_offset_digit0 = std::fmodf(score, 10) * uv;
-	float score_offset_digit1 = std::fmodf(score / 10, 10) * uv;
-	float score_offset_digit2 = std::fmodf(score / 100, 10) * uv;
+	float score_offset_digit0 = std::fmodf(score, 10) * score_uv;
+	float score_offset_digit1 = std::fmodf(score / 10, 10) * score_uv;
+	float score_offset_digit2 = std::fmodf(score / 100, 10) * score_uv;
 
 	if (gt_points->digit_0.new_offset != score_offset_digit0) { // need to flip
 		// prepare flip
 		d0_shown_offset = gt_points->digit_0.old_offset;
 		gt_points->digit_0.new_offset = score_offset_digit0;
-		gt_points->digit_0.old_offset = score_offset_digit0 - uv;
+		gt_points->digit_0.old_offset = score_offset_digit0 - score_uv;
 		// flip
 		d0_flip - 1;
 		gt_points->digit_0.flip = 1;
@@ -204,7 +204,7 @@ void ScoreDisplay::SplitSetScoreDigits(int score, gt_tracker* gt_points) {
 																// prepare flip
 		d1_shown_offset = gt_points->digit_1.old_offset;
 		gt_points->digit_1.new_offset = score_offset_digit1;
-		gt_points->digit_1.old_offset = score_offset_digit1 - uv;
+		gt_points->digit_1.old_offset = score_offset_digit1 - score_uv;
 		// flip
 		d1_flip - 1;
 		gt_points->digit_1.flip = 1;
@@ -227,7 +227,7 @@ void ScoreDisplay::SplitSetScoreDigits(int score, gt_tracker* gt_points) {
 																// prepare flip
 		d2_shown_offset = gt_points->digit_2.old_offset;
 		gt_points->digit_2.new_offset = score_offset_digit2;
-		gt_points->digit_2.old_offset = score_offset_digit2 - uv;
+		gt_points->digit_2.old_offset = score_offset_digit2 - score_uv;
 		// flip
 		d2_flip - 1;
 		gt_points->digit_2.flip = 1;
