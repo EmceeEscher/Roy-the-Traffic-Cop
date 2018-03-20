0bfda06983f561aca9a9e2e3c6d4289b2c3d74cd
EmceeEscher <n0b0b@ugrad.cs.ubc.ca>
2018-03-19 19:47:49 -0700

    got it working

--- src/score_display.hpp
+++ src/score_display.hpp
@@ -2,34 +2,6 @@
 
 #include "common.hpp"
 
-/*
-class ScoreDisplay : public Renderable
-{
-	static Texture score_tex;
-
-public:
-
-	// Creates instance
-	bool init();
-
-	// Split and set digits for texture offset
-	void SplitSetScoreDigits(int score, gt_tracker* gt_points);
-
-	// Renders the score
-	void draw(const mat3& projection) override;
-	
-	// increments the score by one (display won't cross 999)
-	void update_score(int score);
-
-private:
-	
-	int m_current_score;
-	vec2 m_scale;
-	vec2 m_position;
-};
-
-*/
-
 class ScoreDisplay : public Renderable
 {
 	static Texture score_tex;
@@ -47,7 +19,6 @@ public:
 	void draw(const mat3& projection) override;
 
 private:
-	time_t m_current_time; // Current date
 	vec2 m_scale;
 	vec2 m_position;
 };
